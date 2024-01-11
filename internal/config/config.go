714b54161ee5c27b4c500bc77c1e0cf94222d627
Rafael Caixeta <caixetadev@gmail.com>
2024-01-11 19:36:21 +0100

    feat: configure godotenv

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,32 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
+type AppConfig struct {
+	SesEmail string
+}
+
+func NewConfig() *AppConfig {
+	return &AppConfig{
+		SesEmail: "",
+	}
+}
+
+func (ac *AppConfig) GetSesEmail() string {
+	sesEmail, ok := os.LookupEnv("SesEmail")
+	if !ok {
+		fmt.Println("")
+		return ac.SesEmail
+	}
+
+	return sesEmail
+}
 
 func init() {
 	err := godotenv.Load(".env")
